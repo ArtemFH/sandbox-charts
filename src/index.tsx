f7d2bd647153b058fcbfcb0efcb52a62e8d94c9c
ArtemFH <artembereza1@mail.ru>
2023-01-09 06:21:10 +0700

    init

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,13 @@ import ReactDOM from "react-dom";
 import {store} from "./store/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
-import {StyledEngineProvider} from "@mui/material/styles";
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <StyledEngineProvider injectFirst>
                 <Provider store={store()}>
                     <App/>
                 </Provider>
-            </StyledEngineProvider>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
--- src/store/template/actions.ts
+++
@@ -1,8 +0,0 @@
-import {ITemplate} from "../../models/ITemplate";
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import templatesApi from "../../api/requests/template"
-
-export const fetchTemplates = createAsyncThunk<ITemplate[]>('template', async () => {
-    const response = await templatesApi.getTemplates()
-    return response.data;
-})
